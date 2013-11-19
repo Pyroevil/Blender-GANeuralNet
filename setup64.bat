bfcf6fb53a9f52dc4b82455a7896c1822afd6508
JF <pyroevil@hotmail.com>
2013-11-19 11:47:27 -0500

    typical batch for compile cython

---
+++ setup64.bat
@@ -0,0 +1,3 @@
+C:\Python33_x64\python.exe setup64.py build_ext --inplace
+
+"C:\Program Files\Microsoft SDKs\Windows\v7.0\Bin\mt.exe" -inputresource:D:\Blender3D\blender-2.68a-windows64\python33.dll;#2 -outputresource:E:\3D\project_neuralnet\build\windows\64bit\nnetga.pyd;#2
