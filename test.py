f06f36d422b7e320693854f5e0e3d8f01a203bb3
JF <pyroevil@hotmail.com>
2013-11-22 15:52:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,21 @@
 import nnetga
+
 nnetga.add_pop()
+print("Pop added")
 nnetga.add_agent(0,1)
+print("Agent added")
 nnetga.add_net(0,2,1,2,1)
-
-a = (((1,1),),)
-b = nnetga.update(a)
-print("Init:",b)
-
-c = [1,-1,1,-1,1,1,1,10,1]
+print("Net added")
+nnetga.info_net(0,0)
+'''
+c = [0.5,0.6,0.7]
 nnetga.insert_chromo(0,c)
-
-a = (((1,1),),)
-b = nnetga.update(a)
-print("1 1:",b)
-
-a = (((0,0),),)
-b = nnetga.update(a)
-print("0 0:",b)
-
-a = (((1,0),),)
-b = nnetga.update(a)
-print("1 0:",b)
+print("Chromo inserted")
 
 a = (((0,1),),)
 b = nnetga.update(a)
 print("0 1:",b)
+
+
+nnetga.info_net(0,0)
+'''
