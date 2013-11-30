590346c2a09b9379c4eb40a3d599c0a29ca0316d
JF <pyroevil@hotmail.com>
2013-11-29 23:22:41 -0500

    add next_gen function test

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import nnetga
 from time import clock
 
-agentNum = 160
+agentNum = 4000
 stime = clock()
 nnetga.add_pop()
 print ("Pop added in: ",clock() - stime,"sec")
@@ -9,16 +9,14 @@ stime = clock()
 nnetga.add_agent(0,agentNum)
 print ("Agent added in: ",clock() - stime,"sec")
 stime = clock()
-nnetga.add_net(0,40,2,60,16)
+nnetga.add_net(0,2,1,2,1)
 print ("net added in: ",clock() - stime,"sec")
-nnetga.info_net(0,0,0)
+nnetga.info_net(0,0,2)
 
-#c = [6,-6,-3,-6,5,-3,10,10,-5]
-c = [0.5] * 7096
+c = [6,-6,-3,-6,5,-3,10,10,-5]
 stime = clock()
 nnetga.insert_chromo(0,c)
 print ("Insert Chromo in: ",clock() - stime,"sec")
-print("Chromo inserted")
 
 a = [0,1]
 a = [a] * agentNum
@@ -29,6 +27,14 @@ b = nnetga.update(a)
 #print("0 1:",b)
 print ("net updated in: ",clock() - stime,"sec")
 
+d = [0] * agentNum
+for i in range(agentNum):
+    d[i] = i
+stime = clock()    
+nnetga.next_gen(0,d)
+print ("new generation in: ",clock() - stime,"sec")
+nnetga.info_net(0,0,2)
 
-#nnetga.info_net(0,0)
+#for i in range(agentNum):
+    #nnetga.info_net(0,i,1)
 
